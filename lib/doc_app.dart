ae8f3d6010114bd5e8cf1b26f26f49f4fd181c81
Ahmed khaled <ak5585060@gmail.com>
2025-08-16 17:32:02 +0300

    create new branch make colorTheming added package screenutil and flutter svg and make routes and add file onboarding_screen

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:task_doc/core/routing/app_route.dart';
+import 'package:task_doc/core/routing/routes.dart';
+import 'package:task_doc/core/theming/colors.dart';
 
 class DocApp extends StatelessWidget {
-  const DocApp({super.key});
+  final AppRoute appRoute;
+  const DocApp({super.key, required this.appRoute});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Doc App',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Documentation App')),
-        body: const Center(child: Text('Welcome to the Documentation App!')),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Doc App',
+        theme: ThemeData(
+          primaryColor: ColorsManager.mainBlue,
+          scaffoldBackgroundColor: Colors.white,
+        ),
+        initialRoute: Routes.onboardingScreen,
+        onGenerateRoute: AppRoute().generateRoute,
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:task_doc/core/routing/app_route.dart';
 import 'package:task_doc/doc_app.dart';
 
 void main() {
-  runApp(const DocApp());
+  runApp(DocApp(appRoute: AppRoute()));
 }
